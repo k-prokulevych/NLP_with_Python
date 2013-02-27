9eb975e572103df9dcfe29f9185e5f1389746d41
anrom7 <anrom7@gmail.com>
2013-02-27 22:33:20 +0200

    TODO added

--- AndrewMelnyk/Мельник А. ПРЛс-12Task 5.10.11.py
+++ AndrewMelnyk/Мельник А. ПРЛс-12Task 5.10.11.py
@@ -1,11 +1,14 @@
-#Андрій Мельник ПРЛс-12 27.02.12
+# TODO
+# There are only two experements.
+# Do you change affix_length and min_stem_length in loop?
+#пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ-12 27.02.12
 import nltk
 from nltk.corpus import brown
-training=brown.tagged_sents(categories='news') #Змінна яка запам'ятовує використання тегів у вже промаркованому тексті
-sent=('Stephane Hessel the former French Resistance fighter whose 2010 manifesto Time for Outrage inspired social protesters in Europe dies aged 95').split() #Обрав довільне речення з яким я працюватиму
+training=brown.tagged_sents(categories='news') #пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ'пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+sent=('Stephane Hessel the former French Resistance fighter whose 2010 manifesto Time for Outrage inspired social protesters in Europe dies aged 95').split() #пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 sent
 ['Stephane', 'Hessel', 'the', 'former', 'French', 'Resistance', 'fighter', 'whose', '2010', 'manifesto', 'Time', 'for', 'Outrage', 'inspired', 'social', 'protesters', 'in', 'Europe', 'dies', 'aged', '95']
-#Використання AffixTagger з різними значеннями affix_length та min_stem_length
+#пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ AffixTagger пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ affix_length пїЅпїЅ min_stem_length
 affix_tagger=nltk.AffixTagger(training,affix_length=-2,min_stem_length=3)
 affix_tagger.tag(sent)
 [('Stephane', 'NN'), ('Hessel', 'NN'), ('the', None), ('former', 'NN'), ('French', 'WDT'), ('Resistance', 'NN'), ('fighter', 'NN'), ('whose', 'NN'), ('2010', None), ('manifesto', 'NP'), ('Time', None), ('for', None), ('Outrage', 'NN'), ('inspired', 'VBN'), ('social', 'JJ'), ('protesters', 'NNS'), ('in', None), ('Europe', 'NN'), ('dies', None), ('aged', None), ('95', None)]
