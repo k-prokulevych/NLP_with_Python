43ca2e85f63ea9a10c88141baf247e99f7ebcb38
anrom7 <anrom7@gmail.com>
2013-02-27 22:24:30 +0200

    added TODO
    Only 'adore' and 'prefer' are always a verb. 

--- IrynaShtanhret/Task 21.py
+++ IrynaShtanhret/Task 21.py
@@ -1,16 +1,21 @@
-#Ірина Штангрет ПРЛс-13
+# TODO
+# Only 'adore' and 'prefer' are always a verb. 
+# 'love' and 'like' are noun/adj...
+#
+
+#пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ-13
 import nltk
 from nltk.corpus import brown
-text=nltk.corpus.brown.words(categories='romance') #Створюємо змінну і заповнюємо її словами з корпусу Brown
+text=nltk.corpus.brown.words(categories='romance') #пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ Brown
 dic=[]
 fs=[]
-verbs=['adore', 'love', 'like', 'prefer'] #Створили дві порожні змінні та змінну зі словами з умови
+verbs=['adore', 'love', 'like', 'prefer'] #пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ
 for (wd,tg) in nltk.corpus.brown.tagged_words():
 	if tg[:2]=='QL':
 		fs.append(wd)
 fs[:20]
 ['well', 'less', 'very', 'most', 'so', 'real', 'less', 'most', 'as', 'highly', 'fundamentally', 'even', 'very', 'as', 'very', 'most', 'how', 'mighty', 'most', 'much']
-#Заповнили порожній список словами теги яких є QL
+#пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ QL
 for i in range(len(text)):
 	if text[i]in fs and text[i+1]in verbs and (text[i]+' '+text[i+1]) not in dic:
 		dic.append(text[i]+' '+text[i+1])
